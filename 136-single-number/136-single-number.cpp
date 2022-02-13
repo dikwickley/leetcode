e5a20db5aff5041e7ca39135ddc444a236358235
Aniket Singh Rawat <31622972+dikwickley@users.noreply.github.com>
2022-02-13 20:44:08 +0530

    Time: 50 ms (11.83%), Space: 16.8 MB (85.86%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,bool> nmap;
+        int res = 0;
         
         for(int i=0;i<nums.size();i++){
-            if(nmap.find(nums[i]) != nmap.end()){
-                //found;
-                nmap[nums[i]] = true;
-            } else {
-                nmap.insert({nums[i],false});
-            }
+            res = res ^ nums[i];
         }
         
-        for(auto it : nmap){
-            cout<<it.first<<"->"<<it.second<<endl;
-            if(!it.second) return it.first;
-        }
-        return 0;
+        
+        return res;
     }
 };
