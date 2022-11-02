293e55bb73fdd553b869a94b15074014f22f5c17
Aniket Singh Rawat <31622972+dikwickley@users.noreply.github.com>
2022-11-02 16:08:11 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (29.45%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -8,8 +8,8 @@ public:
             x /= 10;
         }
         
-        if(y > ((long long)1<<31) - 1) return 0;
-        if(y < -1*((long long)1<<31) ) return 0;
+        if(y > INT_MAX) return 0;
+        if(y < INT_MIN ) return 0;
         
         return y;
         
