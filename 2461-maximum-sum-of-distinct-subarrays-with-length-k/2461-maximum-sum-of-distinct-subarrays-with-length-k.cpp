86154360b1fccf5faa96f60a9f2fd3ccace1c9ab
Aniket Singh Rawat <31622972+dikwickley@users.noreply.github.com>
2022-11-06 09:59:42 +0530

    Time: 534 ms (12.50%), Space: 89.8 MB (12.50%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,51 +1,26 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        set<int> s;
-        unordered_map<int,int> m;
-        for(int i=0;i<k;i++) {
-            s.insert(nums[i]);
-            if(m.count(nums[i])==0) m.insert({nums[i],1});
-            else m[nums[i]] +=1;
-        }
-        int n = nums.size();
-        vector<long long> prefix(n);
-        prefix[0] = nums[0];
-        for(int i=1;i<n;i++){
-            prefix[i] = nums[i]+prefix[i-1];
-        }
-
-        int left = 0;
-        int right = k-1;
-        
+        long long ans = 0;
+        unordered_map<int, int> mp;
+        int i = 0;
         long long sum = 0;
-        if(s.size()==k) {
-            sum = prefix[k-1];
+        while(i < k  && i < nums.size()){
+            mp[nums[i]]++;
+            sum += nums[i];
+            i++;
         }
-        
-        
-        
-        for(int i=k;i<n;i++){
+        if(mp.size() == k) ans = sum;
+        while(i < nums.size()){
+            mp[nums[i]]++;
+            mp[nums[i-k]]--;
+            if(mp[nums[i-k]] == 0) mp.erase(nums[i-k]);
             
-
-            s.erase(nums[left]);
-            m[nums[left]] -= 1;
-            if(m[nums[left]]>0)
-                s.insert(nums[left]);
-            left++;
-            right++;
-            
-            s.insert(nums[right]);
-            if(m.count(nums[right])==0) m.insert({nums[right],1});
-            else m[nums[right]] +=1;
-            
-            if(s.size()==k){
-                cout<<right<<" "<<left<<"\n";
-                sum = max(sum, (prefix[right]-prefix[left-1]));
-            }
+            sum += nums[i];
+            sum -= nums[i-k];
+            if(mp.size() == k) ans = max(ans, sum);
+            i++;
         }
-        
-        return sum;
-        
+        return ans;
     }
 };
