9e5b09e4a6ebfd810a01ac1ba159fbd7eebbbea1
Aniket Singh Rawat <31622972+dikwickley@users.noreply.github.com>
2022-11-09 21:46:26 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (45.94%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -16,7 +16,7 @@ public:
         return dp[i][j];
     }
     
-    int uniquePathsWithObstacles(vector<vector<int>>& o) {
+    int mem_uniquePathsWithObstacles(vector<vector<int>>& o) {
         
         int n = o.size();
         int m = o[0].size();
@@ -26,5 +26,41 @@ public:
         
         vector<vector<int>> dp(n, vector<int>(m,-1));
         return rec(o, n-1, m-1, dp);
+    }
+    
+    int uniquePathsWithObstacles(vector<vector<int>>& o) {
+        int n = o.size();
+        int m = o[0].size();
+        
+        if(o[n-1][m-1]==1) return 0;
+        if(o[0][0]==1) return 0;
+        
+        vector<vector<int>> dp(n, vector<int>(m,0));
+        
+        
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                
+                if(i==0 && j==0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                
+                if(o[i][j]==1){ 
+                    dp[i][j] = 0;
+                    continue;
+                }
+                
+                int left = 0, right = 0;
+                
+                if(i>0) left = dp[i-1][j];
+                if(j>0) right = dp[i][j-1];
+                
+                dp[i][j] = left+right;
+            }
+        }
+        
+        return dp[n-1][m-1];
     }
 };
