bf331cb06e822a1f4d347299ecf43f35369bb66f
Aniket Singh Rawat <31622972+dikwickley@users.noreply.github.com>
2022-11-09 21:41:26 +0530

    Time: 4 ms (30.29%), Space: 6.5 MB (29.65%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -12,10 +12,35 @@ public:
         
         return dp[i][j];
     }
-    int uniquePaths(int m, int n) {
+    int mem_uniquePaths(int m, int n) {
         
         vector<vector<int>> dp(n, vector<int>(m,-1));
         
         return rec(n-1, m-1, dp);
+    }
+    
+    //tabulation
+    int uniquePaths(int m, int n){
+        vector<vector<int>> dp(n, vector<int>(m,0));
+        
+        dp[0][0] = 1; 
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                
+                if(i==0 && j==0) continue;
+                
+                int r=0, l=0;
+                
+                if(i>0) r = dp[i-1][j]; 
+                if(j>0) l = dp[i][j-1];
+                
+                dp[i][j] = r + l;
+                
+            }
+        }
+        
+        return dp[n-1][m-1]; 
+        
     }
 };
