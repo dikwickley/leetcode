ef947ea5a56f1e476dbd24881d2aaa079e52658a
Aniket Singh Rawat <31622972+dikwickley@users.noreply.github.com>
2022-11-09 21:46:42 +0530

    Time: 10 ms (21.19%), Space: 7.9 MB (45.94%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    int rec(vector<vector<int>>& o, int i, int j, vector<vector<int>>&dp){
+//     int rec(vector<vector<int>>& o, int i, int j, vector<vector<int>>&dp){
         
         
         
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0 ) return 0;
+//         if(i==0 && j==0) return 1;
+//         if(i<0 || j<0 ) return 0;
         
-        if(o[i][j]==1) return 0;
+//         if(o[i][j]==1) return 0;
         
-        if(dp[i][j]!=-1) return dp[i][j];
+//         if(dp[i][j]!=-1) return dp[i][j];
         
-        dp[i][j] = rec(o,i-1,j,dp) + rec(o,i,j-1,dp);
+//         dp[i][j] = rec(o,i-1,j,dp) + rec(o,i,j-1,dp);
         
-        return dp[i][j];
-    }
+//         return dp[i][j];
+//     }
     
-    int mem_uniquePathsWithObstacles(vector<vector<int>>& o) {
+//     int mem_uniquePathsWithObstacles(vector<vector<int>>& o) {
         
-        int n = o.size();
-        int m = o[0].size();
+//         int n = o.size();
+//         int m = o[0].size();
         
-        if(o[n-1][m-1]==1) return 0;
-        if(o[0][0]==1) return 0;
+//         if(o[n-1][m-1]==1) return 0;
+//         if(o[0][0]==1) return 0;
         
-        vector<vector<int>> dp(n, vector<int>(m,-1));
-        return rec(o, n-1, m-1, dp);
-    }
+//         vector<vector<int>> dp(n, vector<int>(m,-1));
+//         return rec(o, n-1, m-1, dp);
+//     }
     
     int uniquePathsWithObstacles(vector<vector<int>>& o) {
         int n = o.size();
