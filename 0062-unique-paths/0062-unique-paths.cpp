fef18b88e76da04490db3805f83afdf854641dff
Aniket Singh Rawat <31622972+dikwickley@users.noreply.github.com>
2022-11-09 21:41:38 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (58.22%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     
-    int rec(int i, int j, vector<vector<int>>&dp){
+//     int rec(int i, int j, vector<vector<int>>&dp){
         
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0 ) return 0;
+//         if(i==0 && j==0) return 1;
+//         if(i<0 || j<0 ) return 0;
         
-        if(dp[i][j]!=-1) return dp[i][j];
+//         if(dp[i][j]!=-1) return dp[i][j];
         
-        dp[i][j] = rec(i-1,j,dp) + rec(i,j-1,dp);
+//         dp[i][j] = rec(i-1,j,dp) + rec(i,j-1,dp);
         
-        return dp[i][j];
-    }
-    int mem_uniquePaths(int m, int n) {
+//         return dp[i][j];
+//     }
+//     int mem_uniquePaths(int m, int n) {
         
-        vector<vector<int>> dp(n, vector<int>(m,-1));
+//         vector<vector<int>> dp(n, vector<int>(m,-1));
         
-        return rec(n-1, m-1, dp);
-    }
+//         return rec(n-1, m-1, dp);
+//     }
     
     //tabulation
     int uniquePaths(int m, int n){
