c659dff2cacede2964fea568056b23a74df1aa0e
Aniket Singh Rawat <31622972+dikwickley@users.noreply.github.com>
2022-11-26 07:16:32 +0530

    Time: 203 ms (38.92%), Space: 45.3 MB (24.03%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,9 +2,12 @@ class Solution {
 public:
     int rec(vector<int>&coins, int i, int amt, vector<vector<int>> &dp){
         int n = coins.size();
-        if(i==n){
+        if(i==0){
             if(amt==0) return 0;
-            else return INT_MAX-1;
+            
+            if(amt%coins[0]==0) return amt/coins[0];
+            
+            return INT_MAX-1;
         }
         
         if(dp[i][amt]!=-1) return dp[i][amt];
@@ -13,7 +16,7 @@ public:
         if(amt>=coins[i]){
             a = 1 + rec(coins, i, amt-coins[i], dp);
         }
-        int b = 0 + rec(coins, i+1, amt, dp);
+        int b = 0 + rec(coins, i-1, amt, dp);
         
         return dp[i][amt] = min(a,b);
     }
@@ -21,8 +24,15 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        int ans = rec(coins, 0, amount, dp);
+        int ans = rec(coins, n-1, amount, dp);
         if(ans==INT_MAX-1) return -1;
         else return ans;
     }
+    
+//     int coinChange(vector<int>&coins, int amount){
+//         int n = coins.size();
+//         vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        
+//         for(int )
+//     }
 };
