c4b20cc94f3d317d2a8ff936177daa085349071e
Aniket Singh Rawat <31622972+dikwickley@users.noreply.github.com>
2022-11-26 07:22:50 +0530

    Time: 343 ms (11.78%), Space: 45.2 MB (24.96%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -21,18 +21,43 @@ public:
         return dp[i][amt] = min(a,b);
     }
     //mem
-    int coinChange(vector<int>& coins, int amount) {
+    // int coinChange(vector<int>& coins, int amount) {
+    //     int n = coins.size();
+    //     vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+    //     int ans = rec(coins, n-1, amount, dp);
+    //     if(ans==INT_MAX-1) return -1;
+    //     else return ans;
+    // }
+    
+    int coinChange(vector<int>&coins, int amount){
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        int ans = rec(coins, n-1, amount, dp);
+        vector<vector<int>> dp(n, vector<int>(amount+1, 0));
+        
+        dp[0][0] = 0;
+        
+        //base case;
+        for(int amt=1;amt<=amount;amt++){
+            if(amt%coins[0]==0) dp[0][amt] = amt/coins[0];
+            else dp[0][amt] = INT_MAX-1;
+        }
+        
+        // i: n->0 in mem
+        // amt: amount -> 0 in mem
+        for(int i=1;i<n;i++){
+            for(int amt=0; amt<=amount; amt++){
+                
+                int a = INT_MAX;
+                if(amt>=coins[i]){
+                    a = 1 + dp[i][amt-coins[i]];
+                }
+                int b = 0 + dp[i-1][amt];
+
+                dp[i][amt] = min(a,b);
+            }
+        }
+
+        int ans =  dp[n-1][amount];
         if(ans==INT_MAX-1) return -1;
         else return ans;
     }
-    
-//     int coinChange(vector<int>&coins, int amount){
-//         int n = coins.size();
-//         vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        
-//         for(int )
-//     }
 };
