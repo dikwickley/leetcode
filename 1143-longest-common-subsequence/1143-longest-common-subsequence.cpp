df917f3f961dc93f15f7dbe90c763138fd3381ec
Aniket Singh Rawat <31622972+dikwickley@users.noreply.github.com>
2022-12-01 16:15:21 +0530

    Time: 30 ms (69.89%), Space: 12.9 MB (59.71%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -7,7 +7,7 @@ public:
         
         if(dp[i][j]!=-1) return dp[i][j];
         
-        int pick = INT_MIN;
+        
         if(text1[i] == text2[j])
             return dp[i][j] =  1 + rec(text1, text2, i-1, j-1, dp);
         else{
@@ -18,12 +18,37 @@ public:
                
         
     }
+    // memoization
+//     int longestCommonSubsequence(string text1, string text2) {
+//         int n = text1.size();
+//         int m = text2.size();
+        
+//         vector<vector<int>> dp(n, vector<int>(m, -1));
+        
+//         return rec(text1, text2, n-1, m-1, dp);
+//     }
+    
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size();
         int m = text2.size();
         
-        vector<vector<int>> dp(n, vector<int>(m, -1));
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+               
         
-        return rec(text1, text2, n-1, m-1, dp);
+        for(int i=1;i<=n;i++){
+            for(int j=1; j<=m;j++){
+                if(text1[i-1] == text2[j-1])
+                    dp[i][j] =  1 + dp[i-1][j-1];
+                else{
+                    int not_pick_1 = dp[i-1][j];
+                    int not_pick_2 = dp[i][j-1];    
+                    dp[i][j] = max(not_pick_1, not_pick_2);
+                }
+            }
+        }
+        
+        
+        
+        return dp[n][m];
     }
 };
